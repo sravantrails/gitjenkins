29335f3e4027b0fce50b533a262b36c95a411414
peddireddysravan <peddireddysravan@gmail.com>
2017-08-11 22:35:05 -0500

    Commmit with testcases

---
+++ src/test/java/servlet/checking/LoginDaoTest.java
@@ -0,0 +1,35 @@
+package servlet.checking;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginDaoTest {
+
+	@Test
+	public void validate() {
+		LoginDao ld = new LoginDao();
+		String u1 = "sravan";
+		String p1 = "sravan";
+		
+
+		boolean t1 = ld.validate(u1, p1);
+		
+
+		Assert.assertEquals("It passed with sravan and sravan", true, t1);
+		
+	}
+	
+	@Test
+	public void validate2() {
+		LoginDao ld = new LoginDao();
+		String u2 = "username";
+		String p2 = "password";
+
+		boolean t2 = ld.validate(u2, p2);
+
+		Assert.assertNotEquals("It is passed with worng credentials", true, t2);
+	}
+	
+}
